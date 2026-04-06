a36303004c60e88df109632c9f87d9b7eaf87b7b
GoranKim0518 <sungmin051830@gmail.com>
2026-04-06 19:00:02 +0900

    fix: fix <HeadMeta/>

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,5 @@
-export default function HeadMeta({ token }: { token?: string }) {
-  return (
-    <>
-      <meta name="google-site-verification" content={token ?? "9mPI4iaZWCnXOa_sUT_9t2ZWxT7WJ4NEOTWsEg3NF2I"} />
-    </>
-  );
+import HeadMeta from "../components/HeadMeta";
+
+export default function Head() {
+  return <HeadMeta token="9mPI4iaZWCnXOa_sUT_9t2ZWxT7WJ4NEOTWsEg3NF2I" />;
 }
